f901b5cf4d93043d37e260de59cdc70e179d58a1
mobolaji00 <mobolaji.akinpelu@yahoo.com>
2022-06-21 18:09:20 -0500

    ready to go to digital ocean23

--- app/config.py
+++ app/config.py
@@ -102,7 +102,7 @@ class Config(object):
             job_defaults = {
                 # 'coalesce': False,
                 # 'max_instances': 3
-            }
+            }#
 
             scheduler = BackgroundScheduler(jobstores=jobstores, executors=executors, job_defaults=job_defaults, timezone='US/Central')
             scheduler.start()
