4b4d5c4825ab537404e11d0e7198858bf60b73d6
mobolaji00 <mobolaji.akinpelu@yahoo.com>
2022-06-25 11:55:33 -0500

    debugging failed json

--- app/views.py
+++ app/views.py
@@ -440,28 +440,31 @@ def placeholder():
 
 @server.route('/authorize')
 def authorize():
-    print("in authorize")
-    # Create flow instance to manage the OAuth 2.0 Authorization Grant Flow steps.
-    flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
-        Config.CLIENT_SECRETS_FILE, scopes=Config.SCOPES)
+    try:
+        print("in authorize")
+        # Create flow instance to manage the OAuth 2.0 Authorization Grant Flow steps.
+        flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
+            Config.CLIENT_SECRETS_FILE, scopes=Config.SCOPES)
 
-    # The URI created here must exactly match one of the authorized redirect URIs
-    # for the OAuth 2.0 client, which you configured in the API Console. If this
-    # value doesn't match an authorized URI, you will get a 'redirect_uri_mismatch'
-    # error.
-    flow.redirect_uri = url_for('oauth2callback', _external=True)
+        # The URI created here must exactly match one of the authorized redirect URIs
+        # for the OAuth 2.0 client, which you configured in the API Console. If this
+        # value doesn't match an authorized URI, you will get a 'redirect_uri_mismatch'
+        # error.
+        flow.redirect_uri = url_for('oauth2callback', _external=True)
 
-    authorization_url, state = flow.authorization_url(
-        # Enable offline access so that you can refresh an access token without
-        # re-prompting the user for permission. Recommended for web server apps.
-        access_type='offline',
-        # Enable incremental authorization. Recommended as a best practice.
-        include_granted_scopes='true')
+        authorization_url, state = flow.authorization_url(
+            # Enable offline access so that you can refresh an access token without
+            # re-prompting the user for permission. Recommended for web server apps.
+            access_type='offline',
+            # Enable incremental authorization. Recommended as a best practice.
+            include_granted_scopes='true')
 
-    # Store the state so the callback can verify the auth server response.
-    session['state'] = state
+        # Store the state so the callback can verify the auth server response.
+        session['state'] = state
 
-    return redirect(authorization_url)
+        return redirect(authorization_url)
+    except Exception as e:
+        traceback.print_exc()
 
 @server.route('/oauth2callback')
 def oauth2callback():
